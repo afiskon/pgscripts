2120b4ed4ff2bec23c177a29cb7b3cbf4542b3e2
Aleksander Alekseev <afiskon@gmail.com>
2016-12-05 17:09:49 +0300

    Update dev-build.sh

--- dev-build.sh
+++ dev-build.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+set -e
+
 CFLAGS="-O0" ./configure --prefix=/home/eax/work/postgrespro/postgresql-install \
     --with-python --enable-tap-tests --enable-cassert --enable-debug \
     --enable-nls --with-openssl --with-perl --with-tcl --with-gssapi \
-    --with-libxml --with-libxslt --with-ldap --with-icu --with-zstd \
-    && \
-  make clean && \
-  echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-' && \
-  make -s -j4 && \
-  echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-' && \
-  make check-world
+    --with-libxml --with-libxslt --with-ldap --with-zstd \
+
+make clean
+
+echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
+
+make -s -j4
+
+echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
+
+make check-world
+
+echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
+
+cd src/test/recovery
+make check
