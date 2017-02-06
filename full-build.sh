2ebb62089b399c5fe5e37ac0a7d676eb8e64ac1b
Aleksander Alekseev <afiskon@gmail.com>
2017-02-06 20:56:35 +0300

    Run `make distclean` instead of `make clean`

--- full-build.sh
+++ full-build.sh
@@ -2,12 +2,12 @@
 
 set -e
 
+make distclean || true
+
 CFLAGS="-O0" ./configure --prefix=/home/eax/work/postgrespro/postgresql-install \
     --with-python --enable-tap-tests --enable-cassert --enable-debug \
     --enable-nls --with-openssl --with-perl --with-tcl --with-gssapi \
     --with-libxml --with-libxslt --with-ldap
-
-make clean
 
 echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
 
