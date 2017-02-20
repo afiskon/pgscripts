a3c2474273a69c69206ae3df6925ff1ede2e73b8
Aleksander Alekseev <afiskon@gmail.com>
2017-02-20 16:05:01 +0300

    quick-build-clang.sh added

---
+++ quick-build-clang.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+make distclean || true
+
+CC=/usr/bin/clang CFLAGS="-O0" ./configure --prefix=/home/eax/work/postgrespro/postgresql-install \
+    --with-python --enable-tap-tests --enable-cassert --enable-debug \
+    --enable-nls --with-openssl --with-perl --with-tcl --with-gssapi \
+    --with-libxml --with-libxslt --with-ldap
+
+echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
+
+make -s -j4
+
+echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
+
+make check
