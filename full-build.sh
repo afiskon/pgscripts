938f91c6993529f44b38d77bb47878b25eebbe0c
Aleksander Alekseev <afiskon@gmail.com>
2017-03-30 15:00:08 +0300

    Avoid using --with-openssl, see https://postgr.es/m/20170329.142112.251668726.horiguchi.kyotaro%40lab.ntt.co.jp

--- full-build.sh
+++ full-build.sh
@@ -4,9 +4,12 @@ set -e
 
 make distclean || true
 
+# --with-openssl not good for Valgrind
+# see https://postgr.es/m/20170329.142112.251668726.horiguchi.kyotaro%40lab.ntt.co.jp
+
 CFLAGS="-O0" ./configure --prefix=/home/eax/work/postgrespro/postgresql-install \
     --with-python --enable-tap-tests --enable-cassert --enable-debug \
-    --enable-nls --with-openssl --with-perl --with-tcl --with-gssapi \
+    --enable-nls --with-perl --with-tcl --with-gssapi \
     --with-libxml --with-libxslt --with-ldap
 
 echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
