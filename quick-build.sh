11b4967cdfb851a1bba0606131e59743df9d7c67
Aleksander Alekseev <afiskon@gmail.com>
2017-06-13 13:53:53 +0300

    Minor changes

--- quick-build.sh
+++ quick-build.sh
@@ -7,6 +7,9 @@ make distclean || true
 # --with-openssl not good for Valgrind
 # see https://postgr.es/m/20170329.142112.251668726.horiguchi.kyotaro%40lab.ntt.co.jp
 
+# to build with python2 instead of python3
+# export PYTHON=/usr/bin/python2
+
 CFLAGS="-O0" ./configure --prefix=/home/eax/work/postgrespro/postgresql-install \
     --with-python --enable-tap-tests --enable-cassert --enable-debug \
     --enable-nls --with-perl --with-tcl --with-gssapi \
