518b1dd6f30b840fb9325240e5deda47d20691fc
Aleksander Alekseev <afiskon@gmail.com>
2017-07-17 11:51:10 +0300

    Disable --with-libxml / --with-libxslt flags

--- quick-build.sh
+++ quick-build.sh
@@ -12,13 +12,15 @@ make distclean || true
 # --with-openssl is not good for Valgrind
 # see https://postgr.es/m/20170329.142112.251668726.horiguchi.kyotaro%40lab.ntt.co.jp
 
+# tests for --with-libxml / --with-libxslt are broken on master -- 2017-07-17
+# see http://afiskon.ru/s/45/7af150f1e3_regression.diffs.txt
+
 # to build with python2 instead of python3
 export PYTHON=/usr/bin/python2
 
 CFLAGS="-O0" ./configure --prefix=$PGINSTALL \
     --with-python --enable-tap-tests --enable-cassert --enable-debug \
-    --enable-nls --with-perl --with-tcl --with-gssapi \
-    --with-libxml --with-libxslt --with-ldap
+    --enable-nls --with-perl --with-tcl --with-gssapi --with-ldap
 
 echo '-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-'
 
