925785998a6860cdceae1713b5cdd92c23e173e6
Aleksander Alekseev <mail@eax.me>
2021-04-26 13:21:29 +0300

    Minor changes

--- static-analysis.sh
+++ static-analysis.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+CHECKER=/Users/eax/opt/checker-279
+
+make distclean || true
 CC=/usr/bin/clang CFLAGS="-O0 -g" \
   ./configure --enable-cassert --enable-debug
 make clean
 rm -r $TMPDIR/clang-report || true
 mkdir $TMPDIR/clang-report
-scan-build -o $TMPDIR/clang-report/ make -j4
+$CHECKER/bin/scan-build -o $TMPDIR/clang-report/ make -j4
