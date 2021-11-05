f4f10b9c7abca61796cb11f6bf760f3a71e62199
Aleksander Alekseev <mail@eax.me>
2021-11-05 14:14:40 +0300

    Update timescale.sh

--- timescale.sh
+++ timescale.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-set -e
+grep "shared_preload_libraries = 'timescaledb'" ~/pginstall/data-master/postgresql.conf
+if [ $? -ne 0 ]
+then
+	echo "shared_preload_libraries = 'timescaledb'" >> ~/pginstall/data-master/postgresql.conf
+fi
 
-echo "shared_preload_libraries = 'timescaledb'" >> ~/pginstall/data-master/postgresql.conf
+psql -c "drop extension if exists timescaledb cascade;"
 ./restart.sh
 psql -c "create extension timescaledb;"
